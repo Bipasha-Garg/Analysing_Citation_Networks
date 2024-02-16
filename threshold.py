c9d9e17ff0cbd4f415832191d81f9da7044d28c1
Bipasha <bipashagarg06@gmail.com>
2024-02-16 12:25:30 +0530

    time filter in plotting and communities

--- threshold.py
+++ threshold.py
@@ -16,9 +16,10 @@ def load_citation_network(file_path):
 
     return graph
 
-def plot(filtered_degrees,filtered_network,degree_threshold):
-    # degree_threshold =10
-    node_colors = [filtered_degrees[node] for node in filtered_network.nodes]
+def plot(filtered_network, degree_threshold):
+    filtered_degrees = dict(filtered_network.degree())
+    node_colors = list(filtered_degrees.values())
+    
     seed = 13648
     pos = nx.spring_layout(filtered_network, seed)
     plt.figure(figsize=(10, 8))
@@ -27,28 +28,34 @@ def plot(filtered_degrees,filtered_network,degree_threshold):
     nx.draw_networkx_edges(filtered_network, pos, alpha=0.1)
     plt.xticks([])
     plt.yticks([])
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=degree_threshold, vmax=max(node_colors)))
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=min(node_colors), vmax=max(node_colors)))
     sm._A = []  # Fix for ScalarMappable
     cbar = plt.colorbar(sm, orientation='vertical', fraction=0.02, pad=0.1)
-    cbar.set_label(f'Node Degree >= {degree_threshold}')
+    cbar.set_label('Node Degree')
+    print(len(filtered_network.nodes))
+    print(len(filtered_network.edges))
+    print(list(filtered_degrees.values()))
     plt.show()
-    
+
 def main():
-    # dataset_path = "./Datasets/cit-HepPh.txt/Cit-HepPh.txt"
-    # dataset_path = "./Datasets/cit-HepPh.txt/sample_100.txt"
     dataset_path = "./Datasets/cit-HepPh.txt/sample_5000.txt"
 
     try:
         citation_network = load_citation_network(dataset_path)
         degree_threshold = 5
-        filtered_nodes = [node for node in citation_network.nodes if citation_network.degree(node) >= degree_threshold]
+        # original_degrees = dict(citation_network.degree())
+        # print("Original degrees:", original_degrees)
+        # print(len(original_degrees))
+        # Ensure that only nodes with degrees greater than or equal to the threshold are included
+        filtered_nodes = [node for node in citation_network.nodes if citation_network.out_degree(node) >= degree_threshold]
         filtered_network = citation_network.subgraph(filtered_nodes)
-        filtered_degrees = dict(filtered_network.degree())
-        plot(filtered_degrees, filtered_network,degree_threshold)
+        # filtered_degrees = dict(filtered_network.degree())
+        # print("Filtered degrees:", filtered_degrees)
+        # print(len(filtered_degrees))
+        plot(filtered_network, degree_threshold)
 
     except Exception as e:
         print("Error:", str(e))
-
 
 if __name__ == "__main__":
     main()
